6da3972a51aebbb0b138c80f8dadbd5dcf6ba797
sagar <sagar.sahay@spektrix.com>
2019-07-08 15:23:04 +0100

    minore refacotring

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,7 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBjSXVmVy6j8z82PVl30TZQkcrlI7HHvpE",
+    apiKey: "ACzaSyBjSXVmVy6j8z82PVl30TZQkcrlI7HHvpE",
     authDomain: "catch-of-the-day-sagar.firebaseapp.com",
     databaseURL: "https://catch-of-the-day-sagar.firebaseio.com"
 });

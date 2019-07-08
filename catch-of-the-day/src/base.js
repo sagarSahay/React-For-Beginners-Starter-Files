8fa461996af0388045aed3d23d3c67eb02ad9767
sagar <sagar.sahay@spektrix.com>
2019-07-08 15:23:31 +0100

    updating config

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,9 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "ACzaSyBjSXVmVy6j8z82PVl30TZQkcrlI7HHvpE",
-    authDomain: "catch-of-the-day-sagar.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-sagar.firebaseio.com"
+    
 });
 
 const base = Rebase.createClass(firebaseApp.database());
